097335f65b938a0f9f885988a3c0e15c27d1bebb
DeskoverNG2024 <119090045+soteliin@users.noreply.github.com>
2024-05-27 18:09:16 -0600

    main commit

---
+++ 2/testimonios-freecodecamp/src/componentes/Testimonio.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Testimonio(){
+    return (
+        <div className='contenedor-testimonio'>
+            <img 
+                className='imagen-testimonio'
+                src={require('../imagenes/testomonio-Emma.png')}
+                alt='Foto de Emma'
+            />
+            <div className='contenedor-texto-testimonio'>
+                
+            </div>
+        </div>
+    );
+}
