4ba79bb77951c273c75371c850241214969f893e
soteliin <soteliin5@gmail.com>
2024-05-28 14:11:05 -0600

    carta1

--- 2/testimonios-freecodecamp/src/App.js
+++ 2/testimonios-freecodecamp/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { Testimonio } from './componentes/Testimonio'
 
 function App() {
   return (
     <div className="App">
-      
+      <div className='contenedor-principal'>
+        <h1>Here is what our alumni say about freeCodeCamp:</h1>
+        <Testimonio />
+      </div>
     </div>
   );
 }
