77586929685d8dce95702c5b8867096bcc04b18c
DeskoverNG2024 <119090045+soteliin@users.noreply.github.com>
2024-05-29 15:07:44 -0600

    tareas completada

--- 5/tareas/src/componentes/ListaDeTareas.js
+++ 5/tareas/src/componentes/ListaDeTareas.js
@@ -7,14 +7,43 @@ function ListaDeTareas() {
 
     const [tareas, setTareas] = useState([]);
 
+    const agregarTarea = tarea => {
+
+        if (tarea.texto.trim()) {
+            tarea.texto = tarea.texto.trim();
+            const tareasActualizadas = [tarea, ...tareas];
+            setTareas(tareasActualizadas);
+        }
+    };
+
+    const eliminarTarea = id => {
+        const tareasActualizadas = tareas.filter(tarea => tarea.id !== id);
+        setTareas(tareasActualizadas);
+    };
+
+    const completarTarea = id => {
+        const tareasActualizadas = tareas.map(tarea => {
+            if (tarea.id === id) {
+                tarea.completada = !tarea.completada;
+            }
+            return tarea;
+        });
+        setTareas(tareasActualizadas);
+    };
+
     return (
         <>
-            <TareaFormulario />
+            <TareaFormulario onSubmit={agregarTarea} />
             <div className='tareas-lista-contenedor'>
                 {
                     tareas.map((tarea) =>
-                        <Tarea texto={tarea.texto}
-                            completada={tarea.completada} />
+                        <Tarea
+                            key={tarea.id}
+                            id={tarea.id}
+                            texto={tarea.texto}
+                            completada={tarea.completada}
+                            completarTarea={completarTarea}
+                            eliminarTarea={eliminarTarea} />
                     )
                 }
             </div>
--- 5/tareas/src/componentes/TareaFormulario.js
+++ 5/tareas/src/componentes/TareaFormulario.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../hojas-de-estilo/TareaFormulario.css'
+import { v4 as uuidv4 } from 'uuid'
 
 function TareaFormulario(props) {
+
+    const [input, setInput] = useState('');
+
+
+    const manejarCambio = e => {
+        setInput(e.target.value);
+    };
+
+    const manejarEnvio = e => {
+        e.preventDefault();
+        const tareaNueva = {
+            id: uuidv4(),
+            texto: input,
+            completada: false
+        };
+
+        props.onSubmit(tareaNueva);
+    };
+
     return (
-        <form>
+        <form className='tarea-formulario'
+            onSubmit={manejarEnvio}>
             <input
                 className='tarea-input'
                 type='text'
                 placeholder='Escribe una tarea'
-                name='texto' />
-            <button className='tarea-boton'>Agregar tarea</button>
+                name='texto'
+                onChange={manejarCambio} />
+            <button className='tarea-boton'
+            >Agregar tarea</button>
         </form>
     )
 }
